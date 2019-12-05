d6f8c15bcab2c7dba3b38ef2205444bf71de60af
Sunspawn <44163703+Sunspawn@users.noreply.github.com>
2019-12-05 18:11:58 +0200

    Main menu placeholder.
    Signed-off-by: Nikolay Babkin <nikolay.babkin@gmail.com>

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 """Main menu of the project.
 """
+login = input("Enter account type:\nCoordinator - 1\n" +
+              "Lecturer - 2\nStudent - 3\n")
+option = input("Enter what you want to do:\nFind question - 1\n")

4d25a74ec0e3a6e3d6ca278a06399673cac71047
420Spawn <nypd36@walla.com>
2019-12-07 22:40:15 +0200

    added semester and special date fields

--- Test.py
+++ Test.py
@@ -2,13 +2,17 @@ class Test:
     """Test class.
 :field serial: The serial number in the database.
 :field year: The year of the test.
+:field semester: the semester the test was taken.
+:field specific_date: first, second or the special date.
 :field course: The serial number of the course the test is for.
 :field questions: A list of the questions in the
 :field maker: The lecturer who wrote the test.
 """
-    def __init__(self, serial, year, course, questions, maker):
+    def __init__(self, serial, year, semester, specific_date, course, questions, maker):
         self.serial = serial
         self.year = year
+        self.semester = semester
+        self.specific_date = specific_date
         self.course = course
         self.questions = list.copy(questions)
         self.maker = maker

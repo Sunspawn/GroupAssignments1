c5bc6d5cf67e71ac15afad8e627529f52a4838a6
420Spawn <nypd36@walla.com>
2019-12-07 17:43:58 +0200

    Update Course.py

--- Course.py
+++ Course.py
@@ -5,7 +5,7 @@ class Course:
 :field proffesor_name: the name of the proffesor.
 :field tests: A list of the tests in the course's history.
 """
-    def __init__(self, serial, name, tests=[]):
+    def __init__(self, serial, course_name, proffesor_name, tests=[]):
         self.serial = serial
         self.course_name = course_name
         self.proffesor_name = proffesor_name

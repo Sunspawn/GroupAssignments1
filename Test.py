9ef979a4b16134f73f957db40f8d5699403896d5
420Spawn <nypd36@walla.com>
2019-12-07 22:40:51 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,8 +2,8 @@ class Test:
     """Test class.
 :field serial: The serial number in the database.
 :field year: The year of the test.
-:field semester: the semester the test was taken.
-:field specific_date: first, second or the special date.
+:field semester: The semester the test was taken.
+:field specific_date: First, second or the special date.
 :field course: The serial number of the course the test is for.
 :field questions: A list of the questions in the
 :field maker: The lecturer who wrote the test.

99e8587c873d09b3e915b47471a480a41d6724a2
Omri Biton <fighterisr@gmail.com>
2019-12-09 00:37:17 +0200

    added read from json file

--- manageData.py
+++ manageData.py
@@ -5,3 +5,9 @@ def writeData(obj):
         tobj = obj.__dict__
         json.dump(tobj,writeToFile)
 
+def readData():
+    data = []
+    with open("database.json", "r+") as readFromFile:
+        for line in readFromFile:
+            data.append(json.loads(line))
+    return data

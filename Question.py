8ccf49951be2379f084af03deb63f8f7c35482ff
Sunspawn <44163703+Sunspawn@users.noreply.github.com>
2019-12-08 12:45:14 +0200

    Updated shit to work with dictionaries for easier searching and adding stuff.
    Signed-off-by: Nikolay Babkin <nikolay.babkin@gmail.com>

--- Question.py
+++ Question.py
@@ -2,10 +2,12 @@ class Question:
     """
 :field serial: The serial number of the question in the database.
 :field file_name: The name of the image/pdf file in the file directory.
-:field test_serial: The serial number of the test the question first appeared
+:field test: The serial number of the test the question first appeared.
 in.
 """
-    def __init__(self, serial, file_name, test_serial):
+    def __init__(self, serial, file_name, test):
+        if type(file_name) is not str:
+            raise TypeError('File name is not a string.')
         self.serial = serial
         self.file_name = file_name
-        self.test_serial = test_serial
+        self.test = test
--- Test.py
+++ Test.py
@@ -1,17 +1,26 @@
+from Question import Question
+
+
 class Test:
     """Test class.
 :field serial: The serial number in the database.
 :field year: The year of the test.
 :field course: The serial number of the course the test is for.
-:field questions: A list of the questions in the
 :field maker: The lecturer who wrote the test.
+:field questions: A list of the questions in the test.
 """
-    def __init__(self, serial, year, course, questions, maker):
+    def __init__(self, serial, year, course, maker, questions={}):
+        for q in questions:
+            if type(q) is not Question:
+                raise TypeError('A non-Question argument in questions.')
         self.serial = serial
         self.year = year
         self.course = course
-        self.questions = list.copy(questions)
-        self.maker = maker
+        self.maker = str(maker)
+        self.questions = dict.copy(questions)
 
     def add_question(self, question):
-        self.questions.append(question)
+        if type(question) is not Question:
+            return False
+        self.questions[question.serial] = question
+        return True

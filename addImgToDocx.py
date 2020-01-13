786a5a0cbbacce8a29b32b8ece29df85ede6cdc9
Nikolay Babkin <nikolay.babkin@gmail.com>
2020-01-13 11:37:22 +0200

    Updated with logging

--- addImgToDocx.py
+++ addImgToDocx.py
@@ -2,11 +2,13 @@
 
 from docx import Document
 from docx.shared import Inches
+from log import log
+
 
 def addImgToDocx():
 	fileName = input("Enter the name of the image file: ")
 	document = Document() 
 	document.add_picture(fileName + ".jpg",width=Inches(15))
-	document.save('docTest.docx')
+	document.save(fileName)
 	print("img added to docTest.docx")
-	
+	log("Save image as document: " + fileName + ".docx", True)
